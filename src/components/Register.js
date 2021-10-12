f78e8f9c6ed1e831a30784638d3ff9ddaff78cc1
Raydan Bala <raydanb@gmail.com>
2021-10-11 21:36:45 -0700

    completed initial Register component

---
+++ src/components/Register.js
@@ -0,0 +1,56 @@
+import { React, useState } from "react";
+import { useHistory } from "react-router";
+
+const Register = ({ setToken }) => {
+  const [ newUsername, setNewUsername ] = useState('');
+  const [ newPassword, setnewPassword ] = useState('');
+  const [ , ] = useState('');
+  const history = useHistory();
+
+  const handleRegisterSubmit = async (event) => {
+    ev.preventDefault();
+
+    try {
+      const registerObj = await callAPI({
+        url: 'users/register',
+        method: 'POST',
+        body: {
+          username: 'newUsername',
+          password: 'newPassword'
+        }
+      });
+
+      if (registerObj.user) {
+        setToken(registerObj.token);
+        if (registerObj.token) {
+          history.push('/');
+        }
+      }
+
+    } catch (error) {
+      console.error(error);
+    }
+
+    setNewUsername('');
+    setnewPassword('');
+  };
+
+  return <>
+    <h2 className='header'>Register</h2>
+    <form onSubmit={handleRegisterSubmit}>
+      <label htmlFor='new-username'>Username:</label>
+      <input placeholder='New Username' type='text' value={newUsername} onChange={
+        ev => setNewUsername(ev.target.value)
+      }></input>
+
+      <label htmlFor='new-password'>Password:</label>
+      <input placeholder='New Password' type='password' /*minLength='8'*/ value={newPassword} onChange={
+        ev => setNewPassword(ev.target.value)
+      }></input>
+
+      <button type='submit'>Register</button>
+    </form>
+  </>
+}
+
+export default Register;
